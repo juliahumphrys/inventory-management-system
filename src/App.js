3735cf9ab490e09bac0c0f9443fd158391dd14b3
Julia <jhumphrys25@jcu.edu>
2024-10-23 14:28:25 -0400

    Got everything back working as expected!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import Inventory from './Components/Inventory';
-import Reports from './Components/Reports';
+import Developers from './Components/Developers';
 import './App.css';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/inventory" element={<Inventory />} />
-          <Route path="/reports" element={<Reports />} />
+          <Route path="/Developers" element={<Developers />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,19 @@
+// src/components/NavBar.js
+import React from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li><a href="/">Home</a></li>
+        <li><a href="/inventory">Inventory</a></li>
+        <li><a href="/reports">Reports</a></li>
+        <li><a href="/developers">Developers Page</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
