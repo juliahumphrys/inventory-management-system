4d2d682216199f8fa92a3664800bf71519e04651
Julia <jhumphrys25@jcu.edu>
2024-10-24 12:25:05 -0400

    made katie's changes to the dropdowns

--- src/Components/Inventory.js
+++ src/Components/Inventory.js
@@ -149,8 +149,21 @@ function Inventory() {
             required
           >
             <option value="" disabled>Select Item Category</option>
-            <option value="Category A">Category A</option>
-            <option value="Category B">Category B</option>
+            <option value="Clothing">Clothing</option>
+            <option value="Animals">Animals</option>
+            <option value="Documents and Money">Documents and Money</option>
+            <option value="Food">Food</option>
+            <option value="Games">Games</option>
+            <option value="Religion">Religion</option>
+            <option value="Hand Props">Hand Props</option>
+            <option value="Liquor Bottles">Liquor Bottles</option>
+            <option value="Medical Instruments">Medical Instruments</option>
+            <option value="Kitchen Items">Kitchen Items</option>
+            <option value="Musical Instruments">Musical Instruments</option>
+            <option value="Holiday">Holiday</option>
+            <option value="Miscellaneous">Miscellaneous</option>
+            <option value="Foam">Lighting</option>
+
           </select>
 
           <select
@@ -159,9 +172,39 @@ function Inventory() {
             required
           >
             <option value="" disabled>Select Item Location</option>
-            <option value="Backstage">Backstage</option>
-            <option value="Control Booth">Control Booth</option>
+            <option value="Greenroom, Prop Bin 1">Greenroom, Prop Bin 1</option>
+            <option value="Greenroom, Prop Bin 2">Greenroom, Prop Bin 2</option>
+            <option value="Greenroom, Prop Bin 3">Greenroom, Prop Bin 3</option>
+            <option value="Greenroom, Prop Bin 4">Greenroom, Prop Bin 4</option>
+            <option value="Greenroom, Prop Bin 5">Greenroom, Prop Bin 5</option>
+            <option value="Greenroom, Prop Bin 6">Greenroom, Prop Bin 6</option>
+            <option value="Greenroom, Prop Bin 7">Greenroom, Prop Bin 7</option>
+            <option value="Greenroom, Prop Bin 8">Greenroom, Prop Bin 8</option>
+            <option value="Greenroom, Prop Bin 9">Greenroom, Prop Bin 9</option>
+            <option value="Greenroom, Prop Bin 10">Greenroom, Prop Bin 10</option>
+            <option value="Greenroom, Prop Bin 11">Greenroom, Prop Bin 11</option>
+            <option value="Greenroom, Prop Bin 12">Greenroom, Prop Bin 12</option>
+            <option value="Greenroom, Prop Bin 13">Greenroom, Prop Bin 13</option>
+            <option value="Greenroom, Prop Bin 14">Greenroom, Prop Bin 14</option>
+            <option value="Greenroom, Prop Bin 15">Greenroom, Prop Bin 15</option>
+            <option value="Greenroom, Prop Bin 16">Greenroom, Prop Bin 16</option>
+            <option value="Greenroom, Prop Bin 17">Greenroom, Prop Bin 17</option>
+            <option value="Greenroom, Prop Bin 18">Greenroom, Prop Bin 18</option>
+            <option value="Greenroom, Prop Bin 19">Greenroom, Prop Bin 19</option>
+            <option value="Greenroom, Prop Bin 20">Greenroom, Prop Bin 20</option>
+            <option value="Workshop">Workshop</option>
             <option value="Storage Unit">Storage Unit</option>
+            <option value="Costume Loft, Rack 1">Costume Loft, Rack 1</option>
+            <option value="Costume Loft, Rack 2">Costume Loft, Rack 2</option>
+            <option value="Costume Loft, Rack 3">Costume Loft, Rack 3</option>
+            <option value="Costume Loft, Rack 4">Costume Loft, Rack 4</option>
+            <option value="Costume Loft, Rack 5">Costume Loft, Rack 5</option>
+            <option value="Costume Loft, Rack 6">Costume Loft, Rack 6</option>
+            <option value="Costume Loft, Rack 7">Costume Loft, Rack 7</option>
+            <option value="Costume Loft, Rack 8">Costume Loft, Rack 8</option>
+            <option value="Costume Loft, Rack 9">Costume Loft, Rack 9</option>
+            <option value="Costume Loft, Rack 10">Costume Loft, Rack 10</option>
+            <option value="Costume Loft, Shelves">Costume Loft, Shelves</option>
           </select>
           <input
             type="number"
