19f37274ce1749747d0e8bebde3bb0b64230ff78
avabrun1 <159275622+avabrun1@users.noreply.github.com>
2024-11-13 22:48:49 -0500

    reinstating option selects

--- src/Components/Inventory.js
+++ src/Components/Inventory.js
@@ -148,19 +148,84 @@ function Inventory() {
             <h5>{editMode ? 'Edit Item' : 'Add a New Item'}</h5>
             {error && <p style={{ color: 'red' }}>{error}</p>}
 
-            <input type="text" placeholder="Item Number" value={newItem.itemNumber} onChange={(e) => setNewItem({ ...newItem, itemNumber: e.target.value })} required />
-            <input type="text" placeholder="Item Name" value={newItem.itemName} onChange={(e) => setNewItem({ ...newItem, itemName: e.target.value })} required />
-            <input type="file" accept="image/*" onChange={handleImageChange} />
+            <input 
+            type="text" 
+            placeholder="Item Number" 
+            value={newItem.itemNumber} 
+            onChange={(e) => setNewItem({ ...newItem, itemNumber: e.target.value })} required />
+
+            <input 
+            type="text" 
+            placeholder="Item Name" 
+            value={newItem.itemName} 
+            onChange={(e) => setNewItem({ ...newItem, itemName: e.target.value })} required />
+
+            <input 
+            type="file" 
+            accept="image/*" 
+            onChange={handleImageChange} />
             {newItem.itemImage && (
-              <img src={newItem.itemImage} alt="Item Preview" style={{ maxWidth: '200px', maxHeight: '200px' }} />
+              <img 
+              src={newItem.itemImage} 
+              alt="Item Preview" 
+              style={{ maxWidth: '200px', maxHeight: '200px' }} />
             )}
 
             <select value={newItem.itemCategory} onChange={(e) => setNewItem({ ...newItem, itemCategory: e.target.value })} required>
               <option value="" disabled>Select Item Category</option>
+              <option value="Clothing">Clothing</option>
+            <option value="Animals">Animals</option>
+            <option value="Documents and Money">Documents and Money</option>
+            <option value="Food">Food</option>
+            <option value="Games">Games</option>
+            <option value="Religion">Religion</option>
+            <option value="Hand Props">Hand Props</option>
+            <option value="Liquor Bottles">Liquor Bottles</option>
+            <option value="Medical Instruments">Medical Instruments</option>
+            <option value="Kitchen Items">Kitchen Items</option>
+            <option value="Musical Instruments">Musical Instruments</option>
+            <option value="Holiday">Holiday</option>
+            <option value="Miscellaneous">Miscellaneous</option>
+            <option value="Foam">Lighting</option>
             </select>
 
             <select value={newItem.itemLocation} onChange={(e) => setNewItem({ ...newItem, itemLocation: e.target.value })} required>
               <option value="" disabled>Select Item Location</option>
+              <option value="In Use for Show">In Use for Show</option>
+            <option value="Greenroom, Prop Bin 1">Greenroom, Prop Bin 1</option>
+            <option value="Greenroom, Prop Bin 2">Greenroom, Prop Bin 2</option>
+            <option value="Greenroom, Prop Bin 3">Greenroom, Prop Bin 3</option>
+            <option value="Greenroom, Prop Bin 4">Greenroom, Prop Bin 4</option>
+            <option value="Greenroom, Prop Bin 5">Greenroom, Prop Bin 5</option>
+            <option value="Greenroom, Prop Bin 6">Greenroom, Prop Bin 6</option>
+            <option value="Greenroom, Prop Bin 7">Greenroom, Prop Bin 7</option>
+            <option value="Greenroom, Prop Bin 8">Greenroom, Prop Bin 8</option>
+            <option value="Greenroom, Prop Bin 9">Greenroom, Prop Bin 9</option>
+            <option value="Greenroom, Prop Bin 10">Greenroom, Prop Bin 10</option>
+            <option value="Greenroom, Prop Bin 11">Greenroom, Prop Bin 11</option>
+            <option value="Greenroom, Prop Bin 12">Greenroom, Prop Bin 12</option>
+            <option value="Greenroom, Prop Bin 13">Greenroom, Prop Bin 13</option>
+            <option value="Greenroom, Prop Bin 14">Greenroom, Prop Bin 14</option>
+            <option value="Greenroom, Prop Bin 15">Greenroom, Prop Bin 15</option>
+            <option value="Greenroom, Prop Bin 16">Greenroom, Prop Bin 16</option>
+            <option value="Greenroom, Prop Bin 17">Greenroom, Prop Bin 17</option>
+            <option value="Greenroom, Prop Bin 18">Greenroom, Prop Bin 18</option>
+            <option value="Greenroom, Prop Bin 19">Greenroom, Prop Bin 19</option>
+            <option value="Greenroom, Prop Bin 20">Greenroom, Prop Bin 20</option>
+            <option value="Workshop">Workshop</option>
+            <option value="Storage Unit">Storage Unit</option>
+            <option value="Costume Loft, Rack 1">Costume Loft, Rack 1</option>
+            <option value="Costume Loft, Rack 2">Costume Loft, Rack 2</option>
+            <option value="Costume Loft, Rack 3">Costume Loft, Rack 3</option>
+            <option value="Costume Loft, Rack 4">Costume Loft, Rack 4</option>
+            <option value="Costume Loft, Rack 5">Costume Loft, Rack 5</option>
+            <option value="Costume Loft, Rack 6">Costume Loft, Rack 6</option>
+            <option value="Costume Loft, Rack 7">Costume Loft, Rack 7</option>
+            <option value="Costume Loft, Rack 8">Costume Loft, Rack 8</option>
+            <option value="Costume Loft, Rack 9">Costume Loft, Rack 9</option>
+            <option value="Costume Loft, Rack 10">Costume Loft, Rack 10</option>
+            <option value="Costume Loft, Shelves">Costume Loft, Shelves</option>
+
             </select>
 
             <input type="number" placeholder="Item Quantity" value={newItem.itemQuantity} onChange={(e) => setNewItem({ ...newItem, itemQuantity: e.target.value })} required />
@@ -168,6 +233,13 @@ function Inventory() {
             <input type="text" placeholder="Item Cost" value={newAdvancedItem.itemCost} onChange={handleCostInputChange} required />
             <select value={newAdvancedItem.itemCondition} onChange={(e) => setNewAdvancedItem({ ...newAdvancedItem, itemCondition: e.target.value })} required>
               <option value="" disabled>Select Item Condition</option>
+              <option value="New">New</option>
+                <option value="Excellent">Excellent</option>
+                <option value="Good">Good</option>
+                <option value="Used">Used</option>
+                <option value="Fair">Fair</option>
+                <option value="Bad">Bad</option>
+                <option value="Disposed">Disposed</option>
             </select>
             <textarea placeholder="Item Description" value={newAdvancedItem.itemDescription} onChange={(e) => setNewAdvancedItem({ ...newAdvancedItem, itemDescription: e.target.value })} required />
 
